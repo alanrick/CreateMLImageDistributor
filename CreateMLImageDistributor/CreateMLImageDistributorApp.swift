bf2d548a338ef28a5a42e48aa56732d51528e776
alanrick <alan@thank-you-partner.com>
2024-11-02 10:07:27 +0100

    Initial Commit

---
+++ CreateMLImageDistributor/CreateMLImageDistributorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateMLImageDistributorApp.swift
+//  CreateMLImageDistributor
+//
+//  Created by Alan Rickayzen on 02.11.24.
+//
+
+import SwiftUI
+
+@main
+struct CreateMLImageDistributorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CreateMLImageDistributorTests/CreateMLImageDistributorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CreateMLImageDistributorTests.swift
+//  CreateMLImageDistributorTests
+//
+//  Created by Alan Rickayzen on 02.11.24.
+//
+
+import Testing
+@testable import CreateMLImageDistributor
+
+struct CreateMLImageDistributorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
